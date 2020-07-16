fb6def68d4cb9bb88d61b68048a35db221ade4ca
MikelAlejoBR <mikelalejobr@outlook.com>
2020-07-16 16:10:04 +0200

    Add intro messages

--- game.cpp
+++ game.cpp
@@ -3,6 +3,8 @@
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
+    std::cout << "You need to enter the correct codes to continue..." << std::endl;
+
     return EXIT_SUCCESS;
 }
